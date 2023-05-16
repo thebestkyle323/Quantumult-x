3eae627f55b8bdc0b60f42a9934d922ab4738682
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2023-05-16 14:15:21 +0800

    Update Mizframa.js

--- Mizframa.js
+++ Mizframa.js
@@ -10,7 +10,7 @@ TG群：https://t.me/ifantasticsgame
 *****************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Mizframa.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/Mizframa.js
 
 [mitm]
 hostname = api.revenuecat.com
