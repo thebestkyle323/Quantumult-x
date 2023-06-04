7aea0817b79f0adf596995f4e77ae3577b9b9be2
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2023-06-04 15:43:55 +0800

    Create chatai.js

---
+++ chatai.js
@@ -0,0 +1,18 @@
+/**********************************************
+
+# Chat AI 解锁 Zen 4 模型，中文回答速度比较慢，英文问答速度快一点
+# Notice：可以用鲁迅和周树人测试是否成功，解锁失败是因为和其他revenuecat脚本冲突，关掉其他revenuecat脚本即可
+# 测试版本v2.9
+#电报频道：https://t.me/ifantasticsgame
+
+# url: https://apps.apple.com/us/app/id1661016696?l=zh
+
+hostname = api.revenuecat.com
+
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://github.com/thebestkyle323/Quantumult-x/blob/main/chatai.js
+
+**********************************************/
+
+
+
+var _0x45d9ec=_0xccbd;(function(_0x51d349,_0x420e53){var _0x412075=_0xccbd,_0x9c2c00=_0x51d349();while(!![]){try{var _0x26360a=parseInt(_0x412075(0x149))/0x1*(parseInt(_0x412075(0x143))/0x2)+-parseInt(_0x412075(0x151))/0x3*(parseInt(_0x412075(0x147))/0x4)+parseInt(_0x412075(0x150))/0x5+parseInt(_0x412075(0x152))/0x6+-parseInt(_0x412075(0x153))/0x7+parseInt(_0x412075(0x14d))/0x8+-parseInt(_0x412075(0x14a))/0x9;if(_0x26360a===_0x420e53)break;else _0x9c2c00['push'](_0x9c2c00['shift']());}catch(_0x5bb54a){_0x9c2c00['push'](_0x9c2c00['shift']());}}}(_0x381f,0x1ca26));function _0x381f(){var _0x5e7a92=['500997gQCjwk','subscriber','9738HzPfXA','normal','com.palligroup.gpt3.yearlyyy','subscriptions','548OAWUUA','2029-05-26T05:05:04Z','37zZrvkt','3360915gZfjtv','app_store','body','1390544XVUihJ','2022-04-09T05:05:04Z','parse','565680QcINoj','2172jcCwBj','1166238kvqLBN'];_0x381f=function(){return _0x5e7a92;};return _0x381f();}function _0xccbd(_0xef8c19,_0x365849){var _0x381f96=_0x381f();return _0xccbd=function(_0xccbda9,_0xa234b6){_0xccbda9=_0xccbda9-0x143;var _0x27578f=_0x381f96[_0xccbda9];return _0x27578f;},_0xccbd(_0xef8c19,_0x365849);}var body=$response[_0x45d9ec(0x14c)],obj=JSON[_0x45d9ec(0x14f)](body);obj[_0x45d9ec(0x154)]['entitlements']={'Advanced':{'expires_date':_0x45d9ec(0x148),'product_identifier':_0x45d9ec(0x145),'purchase_date':'2022-04-09T05:05:04Z'}},obj[_0x45d9ec(0x154)][_0x45d9ec(0x146)]={'com.palligroup.gpt3.yearlyyy':{'billing_issues_detected_at':null,'expires_date':_0x45d9ec(0x148),'is_sandbox':![],'original_purchase_date':_0x45d9ec(0x14e),'period_type':_0x45d9ec(0x144),'purchase_date':_0x45d9ec(0x14e),'store':_0x45d9ec(0x14b),'unsubscribe_detected_at':null}},body=JSON['stringify'](obj),$done({'body':body});
