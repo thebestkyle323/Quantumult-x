bd671ca75b7fe86885a6c693ab382e940573b89d
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2023-07-19 13:53:29 +0800

    Create xiguanguanjia.js

---
+++ xiguanguanjia.js
@@ -0,0 +1,41 @@
+/*
+
+习惯管家：https://apps.apple.com/app/id1253577148
+脚本功能：一次性解锁
+交流频道：https://t.me/Ifantasticsgame
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/xiguanguanjia.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/xiguanguanjia.js
+
+[MITM]
+hostname = api.revenuecat.com
+
+*/
+const guding = {};
+const guding6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  guding.headers = $request.headers;
+} else if (guding6 && guding6.subscriber) {
+  guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
+  guding6.subscriber.entitlement = guding6.subscriber.entitlement || {};
+  const app = 'gd';const list = {'gd':{name: 'habitminder_premium', id: 'habitminder.yearlysubscription'}};
+  const data = {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"};
+for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
+guding6.subscriber.subscriptions[list[i].id] = data;
+guding6.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+guding6.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+                break;
+          }
+    }
+    guding.body = JSON.stringify(guding6);
+}
+$done(guding);
