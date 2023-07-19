2ac5660e8c2c57bae67e7f0eb8cb7b1d743b8a47
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2023-07-19 14:49:33 +0800

    Create OffScreen.js

---
+++ OffScreen.js
@@ -0,0 +1,33 @@
+/*
+
+OffScreen：https://apps.apple.com/app/id1474340105
+脚本功能：解锁VIP
+交流频道：https://t.me/Ifantasticsgame
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/OffScreen.js
+
+[MITM]
+hostname = api.revenuecat.com
+
+*/
+var guding = JSON.parse($response.body);
+guding.subscriber.entitlements = {
+  "Entitlement.Pro": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "product_identifier": "tech.miidii.offscreen.subscription.year.intro.first_year_discount",
+    "purchase_date": "2023-02-23T02:33:33Z"
+  }
+};
+guding.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
+guding.subscriber.subscriptions = {
+  "tech.miidii.offscreen.subscription.year.intro.first_year_discount": {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"
+  }
+};
+$done({ body: JSON.stringify(guding) });
