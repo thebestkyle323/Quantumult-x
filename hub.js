40fdc31724908af0bdb8062353aa31d9085dc387
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2023-10-15 11:33:49 +0800

    Update hub.js

--- hub.js
+++ hub.js
@@ -1,3 +1,4 @@
 # 远程 github js脚本
-https://raw.githubusercontent.com/thebestkyle323/hublist/main/hub.js url script-response-body https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
+[rewrite_local]
+;https://raw.githubusercontent.com/thebestkyle323/hublist/main/hub.js url script-response-body https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
 
