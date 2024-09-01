4db0353bc0a1f092fa52033ac7ddd80ad1106fa4
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2024-09-01 21:19:32 +0800

    更新 mnxzj.js

--- mnxzj.js
+++ mnxzj.js
@@ -1,1 +1,21 @@
+/*******************************
+脚本功能：迷你小组件-解锁永久会员
+TG频道群：https://t.me/iFantasticsgame
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+********************************
+[rewrite_local]
+^https:\/\/fufei\.haitubangong\.com\/api\/client\/info(.?)+ url script-response-body https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/mnxzj.js
+[mitm] 
+hostname = fufei.haitubangong.com
+*******************************/
 
+let obj =  JSON.parse($response.body);
+
+let vipInfo = {
+   "forever" : 1,
+   "vipname" : "永久会员",
+   "expired_at" : "2999-01-01 00:00:00"
+}
+obj.data.vip = vipInfo;
+
+$done({body: JSON.stringify(obj)});
