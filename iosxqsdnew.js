e276fa879b0a77b72c5fd4689cec831994def90b
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2024-09-15 17:05:07 +0800

    Create iosxqsdnew.js

---
+++ iosxqsdnew.js
@@ -0,0 +1,24 @@
+/*******************************
+
+脚本功能：心情树洞——解锁VIP
+TG频道群：https://t.me/iFantasticsgame
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/iosxqsdnew.sanwubeixin.cn\/ct\/user\/info url script-response-body https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/iosxqsdnew.js
+
+[mitm] 
+
+hostname = iosxqsdnew.sanwubeixin.cn
+
+*******************************/
+
+var body = $response.body.replace(/isVip":\w+/g,'isVip":true')
+.replace(/vip_time":\d+/g,'vip_time":8888888888')
+.replace(/vip_type":\d/g,'vip_type":1')
+.replace(/vip_money":\d/g,'vip_money":1')
+.replace(/username":".*?"/g,'username":"iOS社区"')
+$done({ body });
