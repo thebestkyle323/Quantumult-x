dfd05fdfe46ac8b7e4e4f11865a322cfe4a087cb
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2024-12-01 11:05:33 +0800

    Create guide.js

---
+++ guide.js
@@ -0,0 +1,14 @@
+/*************************************
+项目名称：简讯
+使用声明：⚠️仅供参考，🈲转载与售卖！
+https://t.me/iSharesubcribe
+脚本说明：解锁VIP，去除广告
+
+**************************************
+
+[rewrite_local]
+^https://api\.tipsoon\.com/api/v1/user/info url script-response-body https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/vip.js
+[mitm]
+hostname = api.tipsoon.com
+
+*************************************/
