ff9cc4b1e7dfb964960d2e875bf941e33326e74d
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2024-12-19 13:55:17 +0800

    Update Reheji.js

--- Reheji.js
+++ Reheji.js
@@ -26,6 +26,7 @@ if (forbiddenAppFound) {
 }
 
 const bundle = {
+  'camp.user.penbook': { name: 'pro', id: 'penbook.lifetime01', cm: 'sjb' },  //Penbook-智能笔记本
   'design.yugen.Flow': { name: 'pro', id: 'design.yugen.Flow.Lifetime', cm: 'sja' },  //Flow-番茄工作/专注计时器
   'com.runbuddy.prod': { name: 'premium', id: 'rb_9999_1y_1y7999', cm: 'sja' },  //Runna-马拉松训练
   'TeleprompterX': { name: 'Pro Upgrade', id: 'TPXOTP', cm: 'sjb' },  //Teleprompter
