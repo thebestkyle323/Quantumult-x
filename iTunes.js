408c73b0feb37e1c5dfe186cff1b179c932b3365
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2025-02-25 19:01:44 +0800

    更新 iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -1,7 +1,10 @@
 /*******************************
-脚本功能：脚本解锁订阅集合
+脚本功能：脚本解锁订阅
+
 频道地址：https://t.me/iSharesubcribe
+
 使用声明：️仅供学习交流, 🈲️商业用途
+
 ********************************
 [rewrite_local]
 ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/thebestkyle323/Quantumult-x/main/iTunes.js
